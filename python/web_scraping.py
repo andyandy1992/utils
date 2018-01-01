4a131cd336318a7a2ef685617576f74d2f86c8a3
asmith26 <asmith26@users.noreply.github.com>
2018-01-01 22:29:51 +0000

    Create web_scraping.py

---
+++ python/web_scraping.py
@@ -0,0 +1,3 @@
+# Great advice on general web scraping: https://opendatascience.com/blog/web-scraping-with-python-part-two-library-overview-of-requests-urllib2-beautifulsoup-lxml-scrapy-and-more/
+
+# For parsing XML, best library = https://github.com/martinblech/xmltodict
