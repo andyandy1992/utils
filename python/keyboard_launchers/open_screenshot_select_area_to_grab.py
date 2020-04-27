df65a182772fa8a41abfbc77ff7d6c2b653526a3
asmith26 <asmith26@users.noreply.github.com>
2020-04-27 18:40:01 +0100

    Adde keyboard launcher for opening screenshot with area to grab.

---
+++ python/keyboard_launchers/open_screenshot_select_area_to_grab.py
@@ -0,0 +1,13 @@
+import time
+
+import pyautogui
+
+pyautogui.press("winleft")
+pyautogui.write("screenshot")
+pyautogui.press("enter")
+time.sleep(0.5)
+pyautogui.press("down")
+pyautogui.press("down")
+pyautogui.press("enter")
+
+
